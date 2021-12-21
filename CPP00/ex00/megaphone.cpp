777bc7c0a5af3522c38971354fb148258e728bfb
jtrancoso <jesus.trancoso.jimenez@gmail.com>
2021-12-21 18:57:47 +0100

    ex00 done

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/21 18:44:46 by marvin            #+#    #+#             */
+/*   Updated: 2021/12/21 18:44:46 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int i = 1;
+	int j;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		while (argv[i])
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				std::cout << (char)std::toupper(argv[i][j]);
+				j++;
+			}
+			i++;
+		}
+	}
+	std::cout << std::endl;
+	return 0;
+}
