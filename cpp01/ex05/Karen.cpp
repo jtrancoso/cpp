0266ec10911af24a53bfa119a6dd168e8cbb09c2
jtrancos <jesus.trancoso.jimenez@gmail.com>
2022-01-20 18:07:44 +0100

    05 started

--- cpp01/ex05/Karen.cpp
+++ cpp01/ex05/Karen.cpp
@@ -6,7 +6,34 @@
 /*   By: jtrancos <jtrancos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 13:45:01 by jtrancos          #+#    #+#             */
-/*   Updated: 2022/01/20 13:45:01 by jtrancos         ###   ########.fr       */
+/*   Updated: 2022/01/20 18:07:31 by jtrancos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Karen.hpp"
+#include <iostream>
+
+void Karen::debug()
+{
+	std::cout << "I always get my burger with extra tomato, two pickles and a bit of mustard" << std::endl;
+}
+
+void Karen::info()
+{
+	std::cout << "I can't believe I always have to remind the people here that the mustard must be on the top bread. It's not that difficult to remember" << std::endl;
+}
+
+void Karen::warning()
+{
+	std::cout << "I believe I deserve free fries and a soda. I'm a great costumer and after all this trouble is only fair to have some compesation for your mistake" << std::endl;
+}
+
+void Karen::error()
+{
+	std::cout << "This is unacceptable, I want to speak to the manager RIGHT NOW." << std::endl;
+}
+
+void Karen::complain(std::string level)
+{
+	
+}
--- cpp01/ex05/Karen.hpp
+++ cpp01/ex05/Karen.hpp
@@ -6,7 +6,22 @@
 /*   By: jtrancos <jtrancos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 13:45:03 by jtrancos          #+#    #+#             */
-/*   Updated: 2022/01/20 13:45:04 by jtrancos         ###   ########.fr       */
+/*   Updated: 2022/01/20 16:58:36 by jtrancos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#include <string>
+
+class Karen
+{
+	public:
+		void complain(std::string level);
+		void (Karen::*x)();
+
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+};
