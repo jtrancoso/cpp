0913c6783b204cd8851b0d34603a8cd2c9c49129
jtrancos <jesus.trancoso.jimenez@gmail.com>
2022-01-21 17:20:24 +0100

    ufgfhdhdsh

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,7 +6,22 @@
 /*   By: jtrancos <jtrancos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 13:50:53 by jtrancos          #+#    #+#             */
-/*   Updated: 2022/01/21 13:50:54 by jtrancos         ###   ########.fr       */
+/*   Updated: 2022/01/21 17:20:05 by jtrancos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+// Canonical Form: constructor, copy constructor, overload and destructor
+
+class Fixed
+{
+	public:
+		Fixed(); //constructor
+		~Fixed(); //destructor
+		Fixed(const Fixed&); //copy constrctor
+		Fixed &operator=(const Fixed &a); //overload
+
+	private:
+
+};
