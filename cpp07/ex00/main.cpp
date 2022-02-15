4a4bcc37d5c06c0e2fb0d955cf09c1eb35116e61
jtrancos <jesus.trancoso.jimenez@gmail.com>
2022-02-15 13:46:59 +0100

    00 done

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,27 @@
 /*   By: jtrancos <jtrancos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 16:30:33 by jtrancos          #+#    #+#             */
-/*   Updated: 2022/02/14 16:38:28 by jtrancos         ###   ########.fr       */
+/*   Updated: 2022/02/15 13:46:32 by jtrancos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "whatever.hpp"
+#include <iostream>
+
+int main()
+{
+	int a = 2;
+	int b = 3;
+	swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
 
--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -6,8 +6,35 @@
 /*   By: jtrancos <jtrancos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 16:30:35 by jtrancos          #+#    #+#             */
-/*   Updated: 2022/02/14 16:38:26 by jtrancos         ###   ########.fr       */
+/*   Updated: 2022/02/15 13:31:13 by jtrancos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T aux;
+	aux = a;
+	a = b;
+	b = aux;
+}
+
+template <typename T>
+T max(T const &x, T const &y)
+{
+	if (y >= x)
+		return y;
+	else
+		return x;
+}
+
+template <typename T>
+T min(T const &x, T const &y)
+{
+	if (y <= x)
+		return y;
+	else
+		return x;
+}
