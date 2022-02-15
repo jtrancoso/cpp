cd7872363724d396e1bd43571cdc55ca1a9740cc
jtrancos <jesus.trancoso.jimenez@gmail.com>
2022-02-15 16:23:21 +0100

    07 finished

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,8 +6,57 @@
 /*   By: jtrancos <jtrancos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 14:36:21 by jtrancos          #+#    #+#             */
-/*   Updated: 2022/02/15 14:36:25 by jtrancos         ###   ########.fr       */
+/*   Updated: 2022/02/15 16:20:01 by jtrancos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <iostream>
+
+template<typename T>
+class Array
+{
+	public:
+		Array(): _n(0)
+		{
+			this->_elements = new T[0];
+		}
+		Array(unsigned int n): _n(n)
+		{
+			this->_elements = new T[n];
+		}
+		Array(Array const &copy): _n(copy.size())
+		{
+			this->_elements = new T[this->_n];
+			for (unsigned int i = 0; i < this->_n; i++)
+				this->_elements[i] = copy._elements[i];
+		}
+		Array &operator=(Array const &rhs)
+		{
+			this->_n = rhs.size();
+			delete [] this->_elements;
+			this->_elements = new T[this->_n];
+			for (unsigned int i = 0; i < this->_n; i++)
+				this->_elements[i] = rhs._elements[i];
+			return *this;
+		}
+		~Array()
+		{
+			delete [] this->_elements;
+		}
+		T &operator[](unsigned int i)
+		{
+			if (i >= this->_n)
+				throw std::exception();
+			else
+				return this->_elements[i];
+		}
+		int size() const
+		{
+			return this->_n;
+		}
+
+	private:
+		unsigned int _n;
+		T *_elements;
+};
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,14 +6,55 @@
 /*   By: jtrancos <jtrancos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 14:36:18 by jtrancos          #+#    #+#             */
-/*   Updated: 2022/02/15 14:36:54 by jtrancos         ###   ########.fr       */
+/*   Updated: 2022/02/15 16:22:54 by jtrancos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 #include <iostream>
 
+void leakss()
+{
+	system("leaks test");
+}
+
 int main()
 {
+	try
+	{
+		Array<int> small;
+		Array<int> big(6);
+
+		std::cout << "Number of elements first array: " << small.size() << std::endl;
+		std::cout << "Number of elements second array: " << big.size() << std::endl;
+
+		big[0] = 4;
+		big[1] = 8;
+		big[2] = 15;
+		big[3] = 16;
+		big[4] = 23;
+		big[5] = 42;
+
+		Array<int> copy(big);
+		small = big;
+		std::cout << "Number of elements copy array: " << copy.size() << std::endl;
+		std::cout << "Number of elements first array: " << small.size() << std::endl;
+		for (int i = 0; i < big.size(); i++)
+			std::cout << "big[" << i << "]: " << big[i] << std::endl;
+
+		Array<char> charr(3);
+
+		charr[0] = 'a';
+		charr[1] = 'b';
+		charr[2] = 'c';
+		for (int i = 0; i < charr.size(); i++)
+			std::cout << "charr[" << i << "]: " << charr[i] << std::endl;
+		
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "Woops, you went over the array size" << std::endl;
+	}
+	//atexit(leakss);
 	return 0;
 }
